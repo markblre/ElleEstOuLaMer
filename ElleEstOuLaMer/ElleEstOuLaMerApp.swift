28ee2e3f7025d98fc51686b250a038200a5285db
Mark Ballereau <mark.b@live.fr>
2025-07-07 12:25:18 +0200

    Initial Commit

---
+++ ElleEstOuLaMer/ElleEstOuLaMerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElleEstOuLaMerApp.swift
+//  ElleEstOuLaMer
+//
+//  Created by Mark Ballereau on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElleEstOuLaMerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
